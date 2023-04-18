b999d39c067becf611dc55f5aaba4a9919d19a94
Bryan Macario Coronado <bryan.macario@grupobalu.com>
2023-04-18 16:43:40 -0600

    Implementación de layout principal para la aplicación. Navbar, MainLayout y Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { MainLayoutComponent } from '@layout/main-layout/main-layout.component';
+import { NavbarComponent } from '@layout/navbar/navbar.component';
+import { FooterComponent } from '@layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CoreModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: 'media',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    /*plugin(function({addUtilities, addComponents, addBase, theme}) {
+      //Estilo de Utilidades
+      const newTypography = {
+        '.footer': {
+          fontFamily: 'Poppins',
+          fontWeight: '600',
+          fontSize: '16px',
+          color: '#868686'
+        }
+      }
+
+      addUtilities(newTypography)
+    })*/
+  ],
 }
